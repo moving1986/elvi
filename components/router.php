d75cc7f37fab7f720ea396ec4ed14398866f1e00
moving1986 <aleksei_mezencev@mail.ru>
2020-02-13 16:19:34 +0700

    pizdec

---
+++ components/router.php
@@ -0,0 +1,21 @@
+<?php 
+class Router {
+
+    private $routers;
+
+    public function __construct()
+    {
+
+
+    }
+
+    public function run() 
+    {
+
+        echo "Работает эта шляпа";
+    }
+
+
+
+
+}
--- index.php
+++ index.php
@@ -2,38 +2,14 @@
 
 include 'config.php';
 
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/router.php');
+
+$router = new Router();
+$router->run();
+
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" src="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <div class="header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/">Главная странница<span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Каталог продукции</a>
-            <a class="nav-item nav-link" href="#">Индивидуальный заказ</a>
-            <a class="nav-item nav-link" href="#">Доставка и оплата</a>
-            <a class="nav-item nav-link" href="#">Магазины</a>
-            <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Контакты</a>
-            </div>
-        </div>
-        </nav>
-    </div>
-    <div class="content">Contetnt</div>
-</div>
-</body>
-</html> 
