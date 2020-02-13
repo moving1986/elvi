efd36f7116bb26b52424a03bc29f3b47fcae8a5a
moving1986 <aleksei_mezencev@mail.ru>
2020-02-13 10:35:54 +0700

    db connect

--- config.php
+++ config.php
@@ -1,1 +1,9 @@
 <?php
+
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DB", "elvi");
+
+
+$connection = mysql_connect(DBHOST, DBUSER, DBPASS, BD) or die ("Нет соеденения с базой данных")ж
