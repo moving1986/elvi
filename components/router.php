61d660072598cc7d0e21df92d4eee16b40d6c6bb
moving1986 <aleksei_mezencev@mail.ru>
2020-02-17 16:01:30 +0700

    Need test run

--- components/router.php
+++ components/router.php
@@ -5,13 +5,15 @@ class Router {
 
     public function __construct()
     {
-        $routesPath = ROOT.
+        $routesPath = ROOT.'/config/routes.php'
+        $this->routes = include($routesPath); 
 
     }
 
     public function run() 
     {
-
+        // if(!empty($_SERVER['REQUEST_URL']))
+        print_r($this->routes);
         echo "Работает эта шляпа";
     }
 
