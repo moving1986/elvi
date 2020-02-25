58507538f085847266b501d780455664b45f4bb3
moving1986 <aleksei_mezencev@mail.ru>
2020-02-25 09:19:43 +0700

    new reles

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
 return array(
+	'' => 'site/index',
 	'news/([0-9]+)' => 'news/view/$1',
 	'news' => 'news/index', 
 
---
+++ views/site/index.php
@@ -0,0 +1,5 @@
+<?php include ROOT.'/views/layouts/header.php'; ?>
+
+<div class="container"><h1>I'ts a main page</h1></div>
+
+<?php include ROOT.'/views/layouts/footer.php'; ?>
